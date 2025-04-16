b430f2746f52212746af068cd24393ed1ac81f3e
mefkek <zenol.mefeg@gmail.com>
2025-04-16 16:31:39 +0200

    Configured cmake for vs code

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,13 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
+    sf::CircleShape c(75);
+    c.setFillColor(sf::Color::Green);
+    c.setOutlineThickness(-5);
+    c.setOutlineColor(sf::Color::Blue);
+    c.setOrigin({75.f, 75.f});
+    c.setPosition({1920/2.f, 1080/2.f});
+
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -16,6 +23,7 @@ int main()
         }
 
         window.clear();
+        window.draw(c);
         window.display();
     }
 }
