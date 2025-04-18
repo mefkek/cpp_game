80224c1e4e4d59facd923586670e0f9b304b6fb2
GoldSky <kacper.zlotnicki@gmail.com>
2025-04-18 22:13:23 +0200

    SceneManager

---
+++ include/Nodes/SceneManager.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <Nodes/Node.hpp>
+
+
+class SceneManager : public Node {
+
+};
+
+
+
---
+++ src/Nodes/SceneManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zlotn on 18.04.2025.
+//
+#include <Nodes/SceneManager.hpp>
+
