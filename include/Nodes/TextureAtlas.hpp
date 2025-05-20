a32abd8e1d075357a6bfc086d856d1b6d3e6be03
Krokietan <kaketan@o2.pl>
2025-05-20 21:21:08 +0200

    TextureAtlas skeleton missing logger

--- include/Nodes/TextureAtlas.hpp
+++ include/Nodes/TextureAtlas.hpp
@@ -5,16 +5,15 @@
 class TextureAtlas {
 public:
     TextureAtlas(const std::filesystem::path&);
-    void set_rect(const std::unique_ptr<sf::Sprite>&, sf::IntRect rect);
+
+    // Here add a logger message if texture not found
+
+    void set_rect(const std::unique_ptr<sf::Sprite>& sprite, sf::IntRect rect);
+
     const std::unique_ptr<sf::Texture>& get_texture();
-
-
 
 private:
     std::unique_ptr<sf::Texture> text;
-
-
-
 };
 
 
--- src/Nodes/TextureAtlas.cpp
+++ src/Nodes/TextureAtlas.cpp
@@ -1,10 +1,18 @@
 #include "Nodes/TextureAtlas.hpp"
 
-
-TextureAtlas::TextureAtlas(const std::filesystem::path&)
-{}
-    void TextureAtlas::set_rect(const std::unique_ptr<sf::Sprite>&, sf::IntRect rect){}
-    const std::unique_ptr<sf::Texture>& TextureAtlas::get_texture(){return nullptr;}
+TextureAtlas::TextureAtlas(const std::filesystem::path& filepath) {
+    text = std::make_unique<sf::Texture>();
+    if (!text->loadFromFile(filepath)) {
+        throw std::runtime_error("Failed to load texture atlas from: " + filepath.string());   // temporary without logger
+    }
+}
+void TextureAtlas::set_rect(const std::unique_ptr<sf::Sprite>& sprite, sf::IntRect rect) {
+    if (!sprite) {
+        sprite -> setTexture(*text);
+        sprite -> setTextureRect(rect);
+    }
+}
+const std::unique_ptr<sf::Texture>& TextureAtlas::get_texture(){return text;}
 
 
 
