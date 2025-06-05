47fdc7e27ae38c84a66191a0fd30e6ec50220ee6
Krokietan <kaketan@o2.pl>
2025-06-05 14:01:38 +0200

    Integrated Label with RenderManager

--- include/Nodes/Label.hpp
+++ include/Nodes/Label.hpp
@@ -1,15 +1,22 @@
 #pragma once
+#include "Nodes/Node.hpp"
 #include <SFML/Graphics.hpp>
 
-class Label {
+class Label : public Node
+{
 public:
-    Label(const std::string& content, const sf::Font& font, unsigned int size);
+    Label(const std::string layer_name, const sf::Font& font, unsigned int size = 30, const std::string& content = "");
 
     void set_position(const sf::Vector2f& position);
     void set_color(const sf::Color& color);
-    sf::Text& get_text();
-    void render(sf::RenderTarget& target);
+    void set_string(const std::string& str);
+    void set_character_size(unsigned int size);
+
+    sf::FloatRect get_global_bounds() const;
+    std::string get_text();
+
+    void update(float) override;
 
 private:
-    sf::Text text;
+    std::shared_ptr<sf::Text> text;
 };
--- src/Nodes/Label.cpp
+++ src/Nodes/Label.cpp
@@ -1,26 +1,43 @@
 #include "Nodes/Label.hpp"
+#include "Nodes/RenderManager.hpp"
+#include "Nodes/Application.hpp"
 
-Label::Label(const std::string& content, const sf::Font& font, unsigned int size)
-    : text(font, "", size)
+Label::Label(const std::string layer_name, const sf::Font& font, unsigned int size, const std::string& content)
 {
-    text.setFont(font);
-    text.setString(content);
-    text.setCharacterSize(size);
-    text.setFillColor(sf::Color::White);
+    text = std::make_shared<sf::Text>(font);
+    text->setString(content);
+    text->setCharacterSize(size);
+    text->setFillColor(sf::Color::White);
+
+    Application::instance().get_manager<RenderManager>()->add_drawable(layer_name, text);
 }
 
 void Label::set_position(const sf::Vector2f& position) {
-    text.setPosition(position);
+    text->setPosition(position);
 }
 
 void Label::set_color(const sf::Color& color) {
-    text.setFillColor(color);
+    text->setFillColor(color);
 }
 
-sf::Text& Label::get_text() {
-    return text;
+void Label::set_string(const std::string& str)
+{
+    text->setString(str);
 }
 
-void Label::render(sf::RenderTarget& target) {
-    target.draw(text);
+void Label::set_character_size(unsigned int size)
+{
+    text->setCharacterSize(size);
 }
+
+sf::FloatRect Label::get_global_bounds() const
+{
+    return text->getGlobalBounds();
+}
+
+std::string Label::get_text()
+{
+    return text->getString();
+}
+
+void Label::update(float) {}
