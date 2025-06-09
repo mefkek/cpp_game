d2404aec5b898516cdfeb9599d2befc5d3e868b3
Krokietan <kaketan@o2.pl>
2025-06-09 21:06:44 +0200

    FPSCounter clenup

--- include/Nodes/FPSCounter.hpp
+++ include/Nodes/FPSCounter.hpp
@@ -5,20 +5,14 @@
 #include <SFML/Graphics.hpp>
 #include <memory>
 
-class FPSCounter : public Node
+class FPSCounter : public Label
 {
 private:
-    sf::Font font;
     float time_passed;
     unsigned int frames;
 
 public:
-    std::shared_ptr<Label> label;
-
-    FPSCounter();
-    void initialize();
-
-    void set_position(sf::Vector2f pos);
+    using Label::Label;
 
     void update(float delta) override;
 };
--- src/Nodes/FPSCounter.cpp
+++ src/Nodes/FPSCounter.cpp
@@ -4,29 +4,6 @@
 #include <iomanip>
 #include <sstream>
 
-FPSCounter::FPSCounter()
-{
-    if(!font.openFromFile("Fonts/ARIAL.TTF"))
-    {
-        throw FileNotFoundException("Fonts/ARIAL.TTF");
-    }
-    /*
-        Pretend that im getting a reference to
-        a render manager and passing sf::Text to
-        UI layer here
-    */
-}
-
-void FPSCounter::initialize()
-{
-    label = add_child<Label>("Debug_ui", font).lock();
-}
-
-void FPSCounter::set_position(sf::Vector2f pos)
-{
-    label->set_position(pos);
-}
-
 void FPSCounter::update(float delta)
 {
     time_passed += delta;
@@ -42,7 +19,7 @@ void FPSCounter::update(float delta)
         float fps = frames / time_passed;
         std::stringstream ss;
         ss << std::fixed << std::setprecision(0) << fps;
-        label->set_string(ss.str());
+        setString(ss.str());
         time_passed = 0;
         frames = 0;
     }
