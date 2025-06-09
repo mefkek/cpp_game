fbfedd1cffd5a8fc414a26f945e9c966dc39c267
Krokietan <kaketan@o2.pl>
2025-06-09 12:15:07 +0200

    Simplify Icon class

--- include/Nodes/Icon.hpp
+++ include/Nodes/Icon.hpp
@@ -3,21 +3,13 @@
 #include "Node.hpp"
 #include <memory>
 
-class Icon : public Node
+class Icon : public virtual Node, public sf::Sprite
 {
 private:
-    std::shared_ptr<sf::Sprite> sprite;
-    std::string layerName;
+    std::string layer_name;
 public:
-    Icon(const std::string& layer);
     Icon(const std::string& layer, const sf::Texture& texture, sf::IntRect rect = {{0, 0}, {-1, -1}});
-
-    void set_position(const sf::Vector2f& position);
-    void set_scale(const sf::Vector2f& scale);
-    void set_texture(const sf::Texture& texture, sf::IntRect rect = {{0, 0}, {-1, -1}});
-
-    sf::FloatRect get__global_bounds() const;
-    sf::Vector2f get_position() const;
+    void initialize() override;
 
     void update(float delta) override;
 };
--- src/Nodes/Icon.cpp
+++ src/Nodes/Icon.cpp
@@ -2,47 +2,18 @@
 #include "Nodes/RenderManager.hpp"
 #include "Nodes/Application.hpp"
 
-Icon::Icon(const std::string& layer) : layerName(layer) {}
-
 Icon::Icon(const std::string& layer, const sf::Texture& texture, sf::IntRect rect)
-    : layerName(layer)
+    : layer_name(layer), sf::Sprite(texture)
 {
-    sprite = std::make_shared<sf::Sprite>(texture);
     if(rect.size.x != -1 && rect.size.y != -1)
     {
-        sprite->setTextureRect(rect);
+        setTextureRect(rect);
     }
-    Application::instance().get_manager<RenderManager>()->add_drawable(layerName, sprite);
 }
 
-void Icon::set_position(const sf::Vector2f& position)
+void Icon::initialize()
 {
-    sprite->setPosition(position);
-}
-
-void Icon::set_scale(const sf::Vector2f& scale)
-{
-    sprite->setScale(scale);
-}
-
-void Icon::set_texture(const sf::Texture& texture, sf::IntRect rect)
-{
-    sprite = std::make_shared<sf::Sprite>(texture);
-    if(rect.size.x != -1 && rect.size.y != -1)
-    {
-        sprite->setTextureRect(rect);
-    }
-    Application::instance().get_manager<RenderManager>()->add_drawable(layerName, sprite);
-}
-
-sf::FloatRect Icon::get__global_bounds() const
-{
-    return sprite->getGlobalBounds();
-}
-
-sf::Vector2f Icon::get_position() const
-{
-    return sprite->getPosition();
+    Application::instance().get_manager<RenderManager>()->add_drawable(layer_name, std::dynamic_pointer_cast<sf::Sprite>(shared_from_this()));
 }
 
 void Icon::update(float delta){}
