0fedbbfefe25cb399737e288ecbeef814de3f030
Krokietan <kaketan@o2.pl>
2025-06-18 11:40:45 +0200

    Background added

---
+++ include/Graphics/Background.hpp
@@ -0,0 +1,20 @@
+// Background.hpp
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+class Background {
+public:
+    Background();
+
+    explicit Background(const std::string& filename);
+
+    void setPosition(float x, float y);
+
+    void draw(sf::RenderWindow& window) const;
+
+private:
+    sf::Texture texture_;
+    sf::Sprite  sprite_;
+};
---
+++ src/Graphics/Background.cpp
@@ -0,0 +1,21 @@
+// Background.cpp
+#include "Graphics/Background.hpp"
+#include <stdexcept>
+
+Background::Background()
+    : texture_()
+    , sprite_(texture_)  {}
+
+Background::Background(const std::string& filename)
+    : Background(){
+    if (!texture_.loadFromFile(filename)) {
+        throw std::runtime_error("Failed to load background " + filename);
+    }
+    sprite_.setTexture(texture_);
+    }
+void Background::setPosition(float x, float y) {
+    sprite_.setPosition(sf::Vector2f(x, y));
+}
+void Background::draw(sf::RenderWindow& window) const {
+    window.draw(sprite_);
+}
